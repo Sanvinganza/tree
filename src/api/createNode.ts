d3fbba5dc6459b5eec8e1d408e3e3601cbba49b0
Anton Shviatsou <anton.shvetsov@mail.ru>
2024-11-26 02:12:11 +0300

    FIX: method in fetch

--- src/api/createNode.ts
+++ src/api/createNode.ts
@@ -2,5 +2,6 @@ export const createNode = (parentNodeId: number, nodeName: string) =>
   fetch(
     `https://test.vmarmysh.com/api.user.tree.node.create?treeName={${
       import.meta.env.VITE_TREE_NAME
-    }}&parentNodeId=${parentNodeId}&nodeName=${nodeName}`
+    }}&parentNodeId=${parentNodeId}&nodeName=${nodeName}`,
+    { method: "POST" }
   );
